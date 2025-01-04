e807db4ab70a68c98be6eb1b95a5a0e4d9764419
yehtetsan1 <yehtetsan.zotefamily@gmail.com>
2025-01-05 00:04:37 +0630

    add routes

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+  } else if (req.url === '/about') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('About Page\n');
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Page Not Found\n');
+  }
 });
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
-  console.log(process.env);
 });
